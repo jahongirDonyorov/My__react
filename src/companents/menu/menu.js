0c330f4d31beaae5a753e19123cd1e814cc9f7b1
Jahon <donyorovjahon@gmail.com>
2022-11-13 11:05:25 +0000

    My__reactnew

--- src/companents/menu/menu.js
+++ src/companents/menu/menu.js
@@ -49,25 +49,7 @@ const Menu = () => {
           
         }
         
-        {/* <div className="menu__link " >
-          <img src={require('../../assets/img/icons/list.png')} alt=""  />
-          <Link to='/'>Заказы</Link>
-        </div>
-
-        <div className="menu__link" >
-          <img src={require('../../assets/img/icons/tavar.png')} alt=""  />
-          <Link to='/'>Товары</Link>
-        </div>
-
-        <div className="menu__link" >
-          <img src={require('../../assets/img/icons/star.png')} alt=""  />
-          <Link to='/'>Отзывы</Link>
-        </div>
-
-        <div className="menu__link active">
-          <img src={require('../../assets/img/icons/karzinka.png')} alt=""  />
-          <Link to='/'>Оформить заказ</Link>
-        </div> */}
+        
 
       </div>
     </div>
